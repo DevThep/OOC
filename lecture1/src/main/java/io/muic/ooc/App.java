1692f69f89c4c6a8d917c55d1351565d608eda4a
Dev Singh <dev.thep@hotmail.com>
2017-01-12 12:36:43 +0700

    lecture 1

---
+++ lecture1/src/main/java/io/muic/ooc/App.java
@@ -0,0 +1,11 @@
+package io.muic.ooc;
+
+import org.apache.commons.lang.StringUtils;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println(StringUtils.capitalize("ssss"));
+    }
+}
